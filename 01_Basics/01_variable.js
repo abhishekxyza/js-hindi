b0adbdc82ae4240342ae322e9e35993a363067d5
Abhishek Sharma <233185@kit.ac.in>
2026-06-23 19:42:39 +0000

    Add basics files 01_variable.js and 02_dataTypes.js

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,15 @@
+//console.log("hello,Abhishek");
+const accountId=144553
+let accountEmail="Abhishek@gmail.com"
+var accountPassword="123456789"
+accountCity="Bombai"
+let accountState;//it will show undefined 
+accountEmail="ac@gmail.com"
+accountPassword="123123"
+accountCity="jaipur"
+//accountId=2//not allowed 
+console.log(accountEmail);
+console.log(accountId);
+console.table( [accountId,accountCity,accountEmail,accountPassword,accountState]);
+
+ 
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version 
+
+// alert(3+3)  //we are using node.js not browser
+console.log(3+3);  
+console.log("hello,Abhishek")
+let name="abhishek"
+let age=20
+let isLoggedIn= false
+let state;
+//number => 2 to power 53
+// bigint
+// String => ""
+//boolean => true ,false
+// null => standalone value // null is a object in the javascript 
+// undefined =>> //it is a type itself
+// symbol => unique
+
+//object
+console.log(typeof null); 
